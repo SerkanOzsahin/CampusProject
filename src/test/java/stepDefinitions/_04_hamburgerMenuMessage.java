1bcef6f730cabec1a3d451816b51ff427d849ddc
NurhayatSoydan <141257552+NurhayatSoydan@users.noreply.github.com>
2023-12-11 21:27:29 +0300

    04 creates

--- src/test/java/stepDefinitions/_04_hamburgerMenuMessage.java
+++ src/test/java/stepDefinitions/_04_hamburgerMenuMessage.java
@@ -7,13 +7,21 @@ import io.cucumber.java.en.When;
 public class _04_hamburgerMenuMessage {
     @Given("As a student in hamburger menu I want to be able to manage my messages")
     public void asAStudentInHamburgerMenuIWantToBeAbleToManageMyMessages() {
+
+
+
     }
 
     @When("the hamburger menu on the homepage you hover over the messaging link")
     public void theHamburgerMenuOnTheHomepageYouHoverOverTheMessagingLink() {
+
+
     }
+
 
     @Then("Student should be able to view and click on the links new message inbox outbox trash")
     public void studentShouldBeAbleToViewAndClickOnTheLinksNewMessageInboxOutboxTrash() {
+
+
     }
 }
