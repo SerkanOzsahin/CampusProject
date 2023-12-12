13091373e77eb6b2c0d615ca16a80e03c4054c12
kadd190704 <tasdemirkader908@gmail.com>
2023-12-12 14:04:50 +0300

    US_05_HamburgerMenu Scenario

---
+++ src/test/java/stepDefinitions/US_05_HamburgerMenu.java
@@ -0,0 +1,24 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class US_05_HamburgerMenu {
+    @Given("the user clicks on the New Message link, they should see the people they can send messages to.")
+    public void theUserClicksOnTheNewMessageLinkTheyShouldSeeThePeopleTheyCanSendMessagesTo() {
+    }
+
+    @When("The user can choose the person to send the message to and write the subject of the message.")
+    public void theUserCanChooseThePersonToSendTheMessageToAndWriteTheSubjectOfTheMessage() {
+    }
+
+    @And("The student should receive a “Success” message when the Send message button is clicked.")
+    public void theStudentShouldReceiveASuccessMessageWhenTheSendMessageButtonIsClicked() {
+    }
+
+    @Then("User can see list of sent messages")
+    public void userCanSeeListOfSentMessages() {
+    }
+}
