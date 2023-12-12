65fc38b06d792b9185fbcf2b7058bdf76e20440f
Serkan <ozsahinserkan@gmail.com>
2023-12-12 20:22:00 +0300

    onlinepayment develop

--- src/test/java/pages/OnlinePaymentPage.java
+++ src/test/java/pages/OnlinePaymentPage.java
@@ -1,4 +1,37 @@
 package pages;
 
-public class OnlinePaymentPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.BD;
+
+public class OnlinePaymentPage extends Parent {
+
+    public OnlinePaymentPage() {
+        PageFactory.initElements(BD.getDriver(), this);
+    }
+
+    @FindBy(id = "mat-input-0")
+    public WebElement username;
+
+    @FindBy(id = "mat-input-1")
+    public WebElement password;
+
+    @FindBy(css = "button[aria-label='LOGIN']")
+    public WebElement signButton;
+
+    @FindBy(xpath = "(//span[@class='mdc-button__label'])[6]")
+    public WebElement hamburgerButton;
+
+    @FindBy(xpath = "(//span[@class='mat-mdc-menu-item-text'])[2]")
+    public WebElement financeButton;
+
+    @FindBy(xpath = "//span[text()='My Finance']")
+    public WebElement myFinanceButton;
+
+    @FindBy(css = "ms-standard-button[icon='eye']")
+    public WebElement eyeButton;
+
+    @FindBy(css = "input[value='STRIPE']")
+    public WebElement stripeButton;
 }
--- src/test/java/stepDefinitions/OnlinePayment.java
+++ src/test/java/stepDefinitions/OnlinePayment.java
@@ -3,17 +3,38 @@ package stepDefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import pages.OnlinePaymentPage;
+import utilities.BD;
+import utilities.ConfigReader;
 
 public class OnlinePayment {
+
+    OnlinePaymentPage op = new OnlinePaymentPage();
+
     @Given("The user opens the hamburger menu and selects Finance")
     public void theUserOpensTheHamburgerMenuAndSelectsFinance() {
+        BD.getDriver().get("https://test.mersys.io/");
+        op.mySendKeys(op.username, ConfigReader.getProperty("username"));
+        op.mySendKeys(op.password, ConfigReader.getProperty("password"));
+        op.myClick(op.signButton);
+        op.myClick(op.hamburgerButton);
+        op.myClick(op.financeButton);
+        op.myClick(op.myFinanceButton);
+
+
+
+
     }
 
     @When("The user enters the payment amount, selects Online Payment and clicks Make Payment")
     public void theUserEntersThePaymentAmountSelectsOnlinePaymentAndClicksMakePayment() {
+
+
     }
 
     @Then("The user receives a payment confirmation")
     public void theUserReceivesAPaymentConfirmation() {
+
+
     }
 }
