cb15858f390cc0af31bf613dedeab37c50202b50
NurhayatSoydan <141257552+NurhayatSoydan@users.noreply.github.com>
2023-12-12 21:59:41 +0300

    04 us

---
+++ src/test/java/stepDefinitions/_04_hamburgerMenuMessage.java
@@ -0,0 +1,16 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class _04_hamburgerMenuMessage {
+    @Given("The user opens the hamburger menu and selects messages")
+    public void theUserOpensTheHamburgerMenuAndSelectsMessages() {
+
+    }
+
+    @Then("Student should be able to view and click on the links new message inbox outbox trash")
+    public void studentShouldBeAbleToViewAndClickOnTheLinksNewMessageInboxOutboxTrash() {
+
+    }
+}
