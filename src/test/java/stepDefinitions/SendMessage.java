9cca345f23a4d1e906a56216c8d2a64367a25b14
kadd190704 <tasdemirkader908@gmail.com>
2023-12-13 17:48:25 +0300

    SendMessage Feature

--- src/test/java/stepDefinitions/SendMessage.java
+++ src/test/java/stepDefinitions/SendMessage.java
@@ -4,11 +4,18 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import pages.SendMessagePage;
+import utilities.BD;
+import utilities.ConfigReader;
 
 public class SendMessage {
-
+     SendMessagePage sp=new SendMessagePage();
     @Given("the user clicks on the New Message link, they should see the people they can send messages to.")
     public void theUserClicksOnTheNewMessageLinkTheyShouldSeeThePeopleTheyCanSendMessagesTo() {
+        BD.getDriver().get("https://test.mersys.io/");
+        sp.mySendKeys(sp.username, ConfigReader.getProperty("username"));
+        sp.mySendKeys(sp.password, ConfigReader.getProperty("password"));
+        sp.myClick(sp.loginButton);
     }
 
     @When("The user can choose the person to send the message to and write the subject of the message.")
