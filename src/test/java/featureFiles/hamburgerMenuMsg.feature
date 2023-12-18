c3762b42e698730505f915846cb8c3acfe4e7b51
NurhayatSoydan <141257552+NurhayatSoydan@users.noreply.github.com>
2023-12-18 19:49:46 +0300

    04 us

---
+++ src/test/java/featureFiles/hamburgerMenuMsg.feature
@@ -0,0 +1,5 @@
+Feature: Hamburger Menu Messages
+
+  Scenario: Hamburger Menu manage my messages
+    Given The user opens the hamburger menu and selects message
+    Then Student should be able to view and click on the link new message inbox outbox trash
---
+++ src/test/java/stepDefinitions/hamburgerMenuMsg.java
@@ -0,0 +1,36 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import pages.hamburgerMenuMsgPage;
+import utilities.BD;
+
+public class hamburgerMenuMsg {
+    hamburgerMenuMsgPage dc = new hamburgerMenuMsgPage();
+
+    @Given("The user opens the hamburger menu and selects message")
+    public void theUserOpensTheHamburgerMenuAndSelectsMessage() {
+        BD.getDriver().get("https://test.mersys.io/");
+        dc.mySendKeys(dc.username, "Student_4");
+        dc.mySendKeys(dc.password, "S12345");
+        dc.myClick(dc.loginBtn);
+        dc.myClick(dc.menu);
+    }
+
+    @io.cucumber.java.en.Then("Student should be able to view and click on the link new message inbox outbox trash")
+    public void studentShouldBeAbleToViewAndClickOnTheLinkNewMessageInboxOutboxTrash() {
+        dc.wait.until(ExpectedConditions.elementToBeClickable(dc.message));
+        dc.myClick(dc.message);
+        dc.myClick(dc.newMessage);
+        dc.myClick(dc.anaMenu);
+//        dc.wait.until(ExpectedConditions.elementToBeClickable(dc.message));
+//        dc.myClick(dc.inbox);
+//        dc.myClick(dc.anaMenu);
+//        dc.wait.until(ExpectedConditions.elementToBeClickable(dc.message));
+//        dc.myClick(dc.outbox);
+//        dc.myClick(dc.anaMenu);
+//        dc.wait.until(ExpectedConditions.elementToBeClickable(dc.message));
+//        dc.myClick(dc.trash);
+//        dc.myClick(dc.anaMenu);
+    }
+}
